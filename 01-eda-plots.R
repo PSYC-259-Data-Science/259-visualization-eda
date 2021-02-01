dfec7d574ff30d525cd0b19cc4210e53ebbf3ed1
John Franchak <franchak@gmail.com>
2021-02-01 15:39:56 -0800

    trimmed examples

--- 01-eda-plots.R
+++ 01-eda-plots.R
@@ -23,34 +23,32 @@ plot_histogram(ds) #Histograms of all continuous variables
 #Vis_dat and vis_miss also give us useful snapshots
 library(visdat)
 vis_dat(ds) #similar to plot_intro, but gives a snapshot of each column
-vis_miss(ds) #visualiza missing
+vis_miss(ds) #visualize missing
 
 #Vis_expect is a little more customizable
 #Visualize both expected values and missing at the same time
-vis_expect(select(ds, por_x), ~.x > 0 & .x < 640)
-vis_expect(select(ds, avg_fps), ~.x %in% c(29.96, 29.97))
+vis_expect(select(ds, por_x:por_y), ~ .x > 0 & .x < 640)
+vis_expect(select(ds, avg_fps), ~ .x %in% c(29.96, 29.97))
 
 #Let's plot our own histogram
 
 #Simplest histogram
-ggplot(data = ds, mapping = aes(x = por_x)) + geom_histogram()
-ds %>% ggplot(mapping = aes(x = por_x)) + geom_histogram() #Or pipe data in
+ggplot(data = ds, aes(x = por_x)) + geom_histogram()
+ds %>% ggplot(aes(x = por_x)) + geom_histogram() #Or pipe data in
 
 #Piping data in let's us filter first
 ds %>% filter(por_x < 640 & por_x > 0) %>% 
-  ggplot(mapping = aes(x = por_x)) + geom_histogram()
+  ggplot(aes(x = por_x)) + geom_histogram()
 
 #Or we can adjust the graph axes to ignore the out of range data
-ds %>% ggplot(mapping = aes(x = por_x)) + geom_histogram() + xlim(0, 640)
+ds %>% ggplot(aes(x = por_x)) + geom_histogram() + xlim(0, 640)
 
 #Altering the bin width might be helpful rather than relying on the default
 ds %>% filter(por_x < 640 & por_x > 0) %>% 
-  ggplot(mapping = aes(x = por_x)) + geom_histogram(binwidth = 10)
-
-ds %>% count(cut_width(por_y, 50))
+  ggplot(aes(x = por_x)) + geom_histogram(binwidth = 10)
 
 #A boxplot might be a better first glance (note change from x to y for mapping)
-ds %>% ggplot(mapping = aes(y = por_x)) + geom_boxplot()
+ds %>% ggplot(aes(x = por_x)) + geom_boxplot()
 ds %>% filter(por_x < 640 & por_x > 0) %>% ggplot(mapping = aes(y = por_x)) + geom_boxplot()
 
 #At this point, I'm convinced -- let's set the impossible por_x and por_y values to NA 
@@ -65,12 +63,6 @@ ds_cleaned <- ds %>% mutate(
 ds_cleaned %>% ggplot(mapping = aes(x = id, y = por_x)) + geom_boxplot()
 #Now we can better see the outliers and distributions in the remaining data
 
-#Or use freq_poly to overlay multiple histogram lines
-ds_cleaned %>%  ggplot(mapping = aes(color = id, x = por_x)) + geom_freqpoly()
-
-#Normalize counts with y = stat(density)
-ds_cleaned %>%  ggplot(mapping = aes(color = id, x = por_x, y = stat(density))) + geom_freqpoly()
-
 #Scatter plot
 ds_cleaned %>% ggplot(mapping = aes(x = por_x, y = por_y)) + geom_point()
 
@@ -82,7 +74,7 @@ ds_cleaned %>%  ggplot(mapping = aes(x = por_x, y = por_y)) + geom_bin2d()
 ds_cleaned %>% 
   ggplot(mapping = aes(x = por_x, y = por_y)) + 
   geom_bin2d() + 
-  facet_wrap(~ id) +
+  facet_wrap("id") +
   xlim(0, 640) + 
   ylim(0, 480)
 
@@ -96,7 +88,7 @@ for (i in ids) {
   ds_cleaned %>% filter(id == i) %>% 
     ggplot(mapping = aes(x = por_x, y = por_y)) + 
     geom_bin2d() + 
-    facet_wrap(~ cond) + 
+    facet_wrap("cond") + 
     xlim(0, 640) + 
     ylim(0,480)
   ggsave(here("eda","individual_xy_plots",paste0(i,".png")))
